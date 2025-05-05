1515fa1cbf46d754daa4c492dd2a64c71ff2aae7
Lucas-Miranda-BR <lucasmirandabr0508@gmail.com>
2025-05-05 14:50:36 -0300

    Exercicios 1.5

--- Array & forEach/Exercicios/6.js
+++ Array & forEach/Exercicios/6.js
@@ -1,2 +1,17 @@
-let array = [5, 3, 8, 1, 2];
-array.sort()
+let array = [];
+
+array[0] = 5;
+array[1] = 3;
+array[2] = 8;
+array[3] = 1;
+array[4] = 2;
+
+array.sort(ordenarNumeros);
+
+function ordenarNumeros(a, b){
+	return a - b
+}
+
+array.forEach(element => {
+    console.log(array.sort(ordenarNumeros));
+});
